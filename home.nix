6dccc4490b6d077e09c57a2ccd592b02099d2677
Luo Tian <lt@basecity.com>
2026-06-03 22:39:53 +0800

    add mosh

--- home.nix
+++ home.nix
@@ -17,6 +17,22 @@
       plugins = [ "git" "z" ];
       theme = "robbyrussell";
     };
+    envExtra = ''
+      # Nix installer hook (if not already somewhere else)
+      if [ -e "$HOME/.nix-profile/etc/profile.d/nix.sh" ]; then
+        . "$HOME/.nix-profile/etc/profile.d/nix.sh"
+      elif [ -e /nix/var/nix/profiles/default/etc/profile.d/nix-daemon.sh ]; then
+        . /nix/var/nix/profiles/default/etc/profile.d/nix-daemon.sh
+      fi
+
+      # Then source HM session vars so home.sessionPath and sessionVariables apply
+      if [ -e "$HOME/.nix-profile/etc/profile.d/hm-session-vars.sh" ]; then
+        . "$HOME/.nix-profile/etc/profile.d/hm-session-vars.sh"
+      fi
+     '';
+      initContent = ''
+        eval "$(devenv hook zsh)"
+      '';
   };
 
   programs.neovim = {
@@ -38,6 +54,7 @@
     pkgs.ripgrep
     pkgs.babashka
     pkgs.devenv
+    pkgs.mosh
   ];
 
   xdg.configFile."nvim" = {
