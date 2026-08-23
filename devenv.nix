63d313c1df72f5a2c0d95aea4af74ce042d0b73c
Luo Tian <lt@basecity.com>
2026-08-23 15:45:36 +0800

    chore: add devenv scripts for apply, update, check

--- devenv.nix
+++ devenv.nix
@@ -1,11 +1,8 @@
 { pkgs, lib, config, inputs, ... }:
 
 {
-  # https://devenv.sh/basics/
-  env.GREET = "devenv";
-
   # https://devenv.sh/packages/
-  packages = [ 
+  packages = [
     pkgs.git
     pkgs.jq
     pkgs.pi-coding-agent
@@ -14,37 +11,23 @@
   # https://devenv.sh/languages/
   languages.nix.enable = true;
 
-  # https://devenv.sh/processes/
-  # processes.dev.exec = "${lib.getExe pkgs.watchexec} -n -- ls -la";
-
-  # https://devenv.sh/services/
-  # services.postgres.enable = true;
-
   # https://devenv.sh/scripts/
-  scripts.hello.exec = ''
-    echo hello from $GREET
+  scripts.apply.exec = ''
+    home-manager switch --flake ~/.nix#tianluo
+  '';
+
+  scripts.update.exec = ''
+    nix flake update
+  '';
+
+  scripts.check.exec = ''
+    nix build .#homeConfigurations.tianluo.homeConfigurations.tianluo
   '';
 
   # https://devenv.sh/basics/
   enterShell = ''
-    hello         # Run scripts directly
-    git --version # Use packages
+    echo "Available scripts: apply, update, check"
   '';
-
-  # https://devenv.sh/tasks/
-  # tasks = {
-  #   "myproj:setup".exec = "mytool build";
-  #   "devenv:enterShell".after = [ "myproj:setup" ];
-  # };
-
-  # https://devenv.sh/tests/
-  enterTest = ''
-    echo "Running tests"
-    git --version | grep --color=auto "${pkgs.git.version}"
-  '';
-
-  # https://devenv.sh/git-hooks/
-  # git-hooks.hooks.shellcheck.enable = true;
 
   # See full reference at https://devenv.sh/reference/options/
 }
